cc3c0682aa2b89c957b68a1654cb25c0cc97ed3b
Apinda Tekula <nerdcod3rx@gmail.com>
2025-03-24 23:38:10 +0000

    refactor:all-tests-are good-to-go

--- number_guess.sh
+++ number_guess.sh
@@ -1,51 +1,71 @@
 #!/bin/bash
+
+# Database command shortcut
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
-secret_number=$((RANDOM % 1000 + 1))
+# Ask for username
+echo -e "\nEnter your username:"
+read USERNAME
 
-GAME_SPLASH(){
-    echo -e "Enter your username:"
-    read username
+# Check if user exists
+USER_INFO=$($PSQL "SELECT user_id, username FROM users WHERE username='$USERNAME'")
 
-    # get user id
-    USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$username'")
-
-    # if it's a new user
-    if [[ -z $USER_ID ]]
-    then
-        # add user
-         # insert the user
-        INSERT_USER_RESULT=$($PSQL "INSERT INTO users(username) VALUES('$username')")
-        if [[ $INSERT_USER_RESULT == 'INSERT 0 1' ]]
-        then
-            echo -e "Welcome, $username! It looks like this is your first time here."
-            # initialize games_played and best_game
-            games_played=0
-            best_game=0
-        fi
-    # it is an already existing user
-    else
-        # get all the info using the USER_ID
-INFO=$($PSQL "SELECT COUNT(user_id) AS games_played, COALESCE(MIN(score), 0) AS best_game, user_id, username FROM users LEFT JOIN games USING(user_id) WHERE user_id=$USER_ID GROUP BY user_id")
-
-# check if user has any games played
-if [[ -z $INFO ]]
+if [[ -z $USER_INFO ]]
 then
-    games_played=0
-    best_game=0
-    echo -e "\nWelcome back, $USERNAME! You have played $games_played games, and your best game took $best_game guesses."
+  # New user
+  echo -e "\nWelcome, $USERNAME! It looks like this is your first time here."
+  
+  # Insert user into database
+  INSERT_USER_RESULT=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
+  
+  # Get new user ID
+  USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
 else
-    while IFS="|" read GAMES_PLAYED BEST_GAME USER_ID USERNAME
-    do
-        games_played=$GAMES_PLAYED
-        best_game=$BEST_GAME
-        echo -e "\nWelcome back, $USERNAME! You have played $games_played games, and your best game took $best_game guesses."
-    done < <(echo "$INFO")
+  # Existing user
+  IFS="|" read USER_ID USERNAME <<< "$USER_INFO"
+
+  # Get game history
+  USER_STATS=$($PSQL "SELECT COUNT(user_id) AS games_played, COALESCE(MIN(score), 0) AS best_game FROM games WHERE user_id=$USER_ID")
+
+  # Read stats into variables
+  IFS="|" read GAMES_PLAYED BEST_GAME <<< "$USER_STATS"
+
+  echo -e "\nWelcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
 
-    fi
+# Generate random number (1-1000)
+SECRET_NUMBER=$(( RANDOM % 1000 + 1 ))
+GUESSES=0
+USER_GUESS=-1
 
-    echo -e "Guess the secret number between 1 and 1000:"
-    read g
-}
-GAME_SPLASH
+echo -e "\nGuess the secret number between 1 and 1000:"
+
+# Game loop
+while [[ $USER_GUESS -ne $SECRET_NUMBER ]]
+do
+  read USER_GUESS
+
+  # Check if input is a valid number
+  if ! [[ "$USER_GUESS" =~ ^[0-9]+$ ]]
+  then
+    echo "That is not an integer, guess again:"
+    continue
+  fi
+
+  ((GUESSES++))
+
+  if [[ $USER_GUESS -lt $SECRET_NUMBER ]]
+  then
+    echo "It's higher than that, guess again:"
+  elif [[ $USER_GUESS -gt $SECRET_NUMBER ]]
+  then
+    echo "It's lower than that, guess again:"
+  fi
+done
+
+# Game won
+echo -e "\nYou guessed it in $GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
+
+# Save game result
+SAVE_RESULT=$($PSQL "INSERT INTO games(user_id, score) VALUES($USER_ID, $GUESSES)")
+
